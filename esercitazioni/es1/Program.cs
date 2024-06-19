0d7608ef6ee5357fd74ba66c8f4ff7b6d20ed87a
mtm90 <mattia22nchi@gmail.com>
2024-06-19 12:54:21 +0200

    prove varie

--- esercitazioni/es1/Program.cs
+++ esercitazioni/es1/Program.cs
@@ -1,5 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
+double radius = 1;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
+
+int a = 5;
+int b = 3;
+if (a + b > 10)
+{
+    Console.WriteLine("The answer is greater than 10");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+}
 /* 
 Questo è un commento multilinea
 posso scrivere su più linee e mi considera sempre lo stesso commento
