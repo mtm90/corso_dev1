5dc7499283d11e8caa95a06b688840b8f8fea13b
mtm90 <mattia22nchi@gmail.com>
2024-06-19 11:58:45 +0200

    commento multilinea

--- esercitazioni/es1/Program.cs
+++ esercitazioni/es1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/* 
+è difficile ricordarsi tutti i comandi quindi è consigliato fare in questo modo allora bla blabl
+*/
