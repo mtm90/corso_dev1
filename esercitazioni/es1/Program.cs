3dbc7e131db52a4e2db1aa9b79f10e3df6a886b1
mtm90 <mattia22nchi@gmail.com>
2024-06-19 12:59:57 +0200

    altre prove

--- esercitazioni/es1/Program.cs
+++ esercitazioni/es1/Program.cs
@@ -17,7 +17,15 @@ else
 {
     Console.WriteLine("The answer is not greater than 10");
 }
+
+int counter = 0;
+do
+{
+  Console.WriteLine($"Hello World! The counter is {counter}");
+  counter++;
+} while (counter < 10);
 /* 
 Questo è un commento multilinea
 posso scrivere su più linee e mi considera sempre lo stesso commento
 */
+
