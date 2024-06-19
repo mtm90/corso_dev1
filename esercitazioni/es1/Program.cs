1f6ee58bfaa0f128833dd9f3d148fec497593a1b
mtm90 <mattia22nchi@gmail.com>
2024-06-19 11:59:40 +0200

    commit inutile

--- esercitazioni/es1/Program.cs
+++ esercitazioni/es1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 /* 
-è difficile ricordarsi tutti i comandi quindi è consigliato fare in questo modo allora bla blabl
+Questo è un commento multilinea
+posso scrivere su più linee e mi considera sempre lo stesso commento
 */
