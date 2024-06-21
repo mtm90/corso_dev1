66c7c4cb44690e78e088e669b3bf70fdb822894c
mtm90 <mattia22nchi@gmail.com>
2024-06-21 11:14:44 +0200

    operatori logici

---
+++ esercitazioni/02-operatori/Program.cs
@@ -0,0 +1,64 @@
+﻿// gli operatori sono utilizzati per eseguire operazioni su uno o più operandi
+
+
+// operatori aritmetici
+
+
+int a = 10;
+
+int b = 20;
+
+int somma = a + b;
+
+int differenza = a - b;
+
+int prodotto = a * b ;
+
+int divisione = a / b;
+
+int modulo = a % b;
+
+
+// operatori di confronto 
+
+
+int c = 30;
+
+int c == a;             // false
+
+int c != a;             // true
+
+int c > a;              // true
+
+int c < a;              // false
+
+int c >= a;             // true
+
+int c <= a;             // false
+
+
+// operatori logici
+
+
+bool x = true;
+
+bool y = false;
+
+// and
+bool z = x && y;        // false
+
+// or
+bool w = x || y;        // true
+
+// not
+bool v = !x;            // false
+
+
+/* operatori di assegnazione 
+
+assegnazione (=)
+assegnazione con addizione(+=)
+assegnazione con sottrazione(-=)
+
+*/
+
