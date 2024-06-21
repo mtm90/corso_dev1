ec881470b02faa1984ad22dccd1423d831ed35b4
mtm90 <mattia22nchi@gmail.com>
2024-06-21 10:43:41 +0200

    tipi i di dati

---
+++ esercitazioni/01-Tipi-di-dati/Program.cs
@@ -0,0 +1,41 @@
+﻿// esempio di dichiarazioni di variabili
+
+int numero;     // dichiaro una variabile di tipo intero
+
+string nome;    // dichiaro una variabile di tipo stringa
+
+bool flag;      // dichiaro una variabile di tipo booleano
+
+                // esempio di inizializzazione di variabili
+
+numero = 10; 
+
+nome = "mario";
+
+flag = false;
+
+                // esempio di dichiarazione ed inizializzazione di variabili
+
+int età = 20;
+
+string cognome = "Rossi";
+
+bool isStudent = false;
+
+                // esempio di dichiarazione e inizializzazione di una costante
+
+const double PI = 3.14159;
+
+                // esempio di dichiarazione e inizializzazione di una variabile con var
+
+var altezza = 1.80;     
+
+                // lo svantaggio è che non è possibile dichiarare una variabile var senza inizializzarla
+
+                // REGOLA IMPORTANTE: una variabile deve essere inizializzata prima di essere utilizzata
+
+                // REGOLA IMPORTANTE: i nomi delle variabili devono essere significativi e autoesplicativi
+
+Console.WriteLine($"Ciao {nome} {cognome}! Come va?");
+
+
