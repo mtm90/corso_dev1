6f4954a2ca115f2828419b24752175f38755ffce
mtm90 <mattia22nchi@gmail.com>
2024-06-25 12:23:51 +0200

    commenti

--- esercitazioni/12-metodo-random/Program.cs
+++ esercitazioni/12-metodo-random/Program.cs
@@ -3,3 +3,7 @@
 Random random = new Random();
 int numero = random.Next(10);
 Console.WriteLine($"Il numero casuale è {numero}");
+
+// new è un costruttore: usiamo qualcosa che nella sua forma non si può usare
+// serve un  nuovo costruttore che crea un istanza utilizzabile da quel costruttore.
+
