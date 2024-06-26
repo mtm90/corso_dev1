b6c1902e98f639c4a9d3edd7a676e18979eadfbf
mtm90 <mattia22nchi@gmail.com>
2024-06-26 12:48:10 +0200

    fizzbuzz

---
+++ esercitazioni/14-indovina-pari-dispari/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Console.Clear();
+Random random = new Random();
+int number = random.Next(11);
+Console.WriteLine("I chose a number. Is the number even or odd?");
+string answer = Console.ReadLine().ToLower();
+bool isEven = number % 2 == 0;
+
+if ((isEven && answer == "even") || (!isEven && answer == "odd"))
+{
+    Console.WriteLine("You won");
+}
+else 
+{
+    Console.WriteLine("You lost");
+}
+
+Console.WriteLine($"The number was {number}");
+
+*/
+
+Console.Clear();
+
+Random random = new Random();
+int computerNumber = random.Next(1,5);
+
+Console.WriteLine("Choose a number between 1 and 5");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("I also chose a number... Do you think the sum of our numbers is even or odd?");
+string answer = Console.ReadLine();
+
+bool sumIsEven = (computerNumber + userNumber) % 2 == 0;
+
+if ((sumIsEven && answer == "even") || (!sumIsEven && answer == "odd"))
+{
+    Console.WriteLine($"You won! I chose {computerNumber} and the sum is {computerNumber + userNumber} ");
+}
+else
+{
+    Console.WriteLine($"You lost! I chose {computerNumber} and the sum is {computerNumber + userNumber}");
+}
+
+
+
+
+
+
---
+++ esercitazioni/15-fizzbuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Console.Clear();
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 3 == 0 && number % 5 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+}
+else if (number % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else if (number % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else 
+{
+    Console.WriteLine(number);
+}
+*/
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"numero: {i} FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"numero: {i} Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"numero: {i} Buzz");
+    }
+    else
+    {
+        Console.WriteLine($"numero: {i}");
+    }
+}
