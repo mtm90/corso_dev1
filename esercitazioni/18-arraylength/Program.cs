229619c7346d4e680e8556f5c0b7e113bb0a45b9
mtm90 <mattia22nchi@gmail.com>
2024-06-28 10:39:52 +0200

    17 e 18

---
+++ esercitazioni/18-arraylength/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] nomi = new string[8];
+nomi[0] = "Mattia";
+nomi[1] = "Allison";
+nomi[2] = "Ginevra";
+nomi[3] = "Daniele";
+nomi[4] = "Sergey";
+nomi[5] = "Silvano";
+nomi[6] = "Matteo";
+nomi[7] = "Sharon";
+
+Console.WriteLine($"Il numero di elementi è {nomi.Length}");
