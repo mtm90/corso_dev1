62ab90eaf05978d2a3b3d638976bd5633d1b7448
mtm90 <mattia22nchi@gmail.com>
2024-07-03 15:32:35 +0200

    dice fixes

--- esercitazioni/30-yahtzee/Program.cs
+++ esercitazioni/30-yahtzee/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Throw 5 dices");
+﻿Console.Clear();
+Console.WriteLine("Let's play Yatzee! Press a key to throw 5 dices");
+Console.ReadKey(true);
 Random random = new Random();
 int[] dadi = new int[5];
     dadi[0] = random.Next(1,7);
@@ -11,8 +13,28 @@ int[] dadi = new int[5];
     {
         Console.WriteLine(i);
     }
-    Console.WriteLine("Which dice do u wanna reroll?");
-    Console.ReadLine();
+    bool keepPlaying = true;
+        while (keepPlaying)
+        {
+        Console.WriteLine("Do u wanna reroll a dice?");
+        string answer = Console.ReadLine()!;
+        if  (answer == "yes")
+            {
+                Console.WriteLine("Which dice do u wanna reroll?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                dadi[number - 1] = random.Next(1,7);
+            }
+        else if (answer == "no")
+            {
+                Console.WriteLine("These are your final dices");
+                foreach (int i in dadi)
+            {
+            Console.WriteLine(i);
+            }
+            keepPlaying = false;
+            }
+        }
+
     
 
 
