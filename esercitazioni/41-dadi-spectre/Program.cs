5e896eb7148f2f6bb3a0e2382400b364f623bde7
mtm90 <mattia22nchi@gmail.com>
2024-07-09 16:07:36 +0200

    last changes

--- esercitazioni/41-dadi-spectre/Program.cs
+++ esercitazioni/41-dadi-spectre/Program.cs
@@ -7,24 +7,14 @@ int computerScore = 100;
 AnsiConsole.Write(new FigletText("Dice Game").Centered().Color(Color.Green));
 AnsiConsole.MarkupLine("[bold]Welcome to the dice game, you'll be playing against me![/]");
 
-// Function to display scores with bar charts
-void DisplayScores(int myScore, int computerScore)
-{
-    AnsiConsole.MarkupLine("[bold]Here are the new scores:[/]");
-    
-    var barChart = new BarChart()
-        .Width(60)
-        .Label("[bold]Scores[/]")
-        .CenterLabel();
-
-    barChart.AddItem("You", myScore, Color.Green);
-    barChart.AddItem("Me", computerScore, Color.Red);
-
-    AnsiConsole.Write(barChart);
-}
-
-// Initial Score Display
-DisplayScores(myScore, computerScore);
+AnsiConsole.MarkupLine("[bold]Here are the new scores:[/]");
+var barChart = new BarChart()
+    .Width(60)
+    .Label("[bold]Scores[/]")
+    .CenterLabel();
+barChart.AddItem("You", myScore, Color.Green);
+barChart.AddItem("Me", computerScore, Color.Red);
+AnsiConsole.Write(barChart);
 
 while (myScore > 0 && computerScore > 0)
 {
@@ -37,20 +27,19 @@ while (myScore > 0 && computerScore > 0)
     int dice3 = random.Next(1, 7);
     int dice4 = random.Next(1, 7);
 
-    // Display User Dice
     var userPanel = new Panel($"[bold]Your dice:[/]\n[green]{dice1}[/], [green]{dice2}[/]");
     userPanel.Header = new PanelHeader("[bold]Your Roll[/]", Justify.Center);
     userPanel.Border = BoxBorder.Double;
     userPanel.Padding = new Padding(1, 1, 1, 1);
+    userPanel.Width = 15;
     AnsiConsole.Write(userPanel);
 
-    // Display Computer Dice
     var computerPanel = new Panel($"[bold]My dice:[/]\n[green]{dice3}[/], [green]{dice4}[/]");
     computerPanel.Header = new PanelHeader("[bold]My Roll[/]", Justify.Center);
     computerPanel.Border = BoxBorder.Double;
     computerPanel.Padding = new Padding(1, 1, 1, 1);
-    AnsiConsole.Write(computerPanel);
     computerPanel.Width = 15;
+    AnsiConsole.Write(computerPanel);
 
     int myPoints = dice1 + dice2;
     int computerPoints = dice3 + dice4;
@@ -67,8 +56,14 @@ while (myScore > 0 && computerScore > 0)
         if (myScore < 0) myScore = 0;
     }
 
-    // Updated Score Display
-    DisplayScores(myScore, computerScore);
+    AnsiConsole.MarkupLine("[bold]Here are the new scores:[/]");
+    barChart = new BarChart()
+        .Width(60)
+        .Label("[bold]Scores[/]")
+        .CenterLabel();
+    barChart.AddItem("You", myScore, Color.Green);
+    barChart.AddItem("Me", computerScore, Color.Red);
+    AnsiConsole.Write(barChart);
 }
 
 if (myScore <= 0)
