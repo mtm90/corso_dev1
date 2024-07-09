4f12eebf361d95489023b72d5e105cdf3a3c188b
mtm90 <mattia22nchi@gmail.com>
2024-07-09 15:59:11 +0200

    dadi spectre

--- esercitazioni/40-sorteggio-squadre/Program.cs
+++ esercitazioni/40-sorteggio-squadre/Program.cs
@@ -39,7 +39,9 @@ List<string> devclass = new List<string>
             
 
         }
+        table1.Width(15);
         AnsiConsole.Write(table1);
+
         var table2 = new Table();
         table2.AddColumn("[bold blue]team 2[/]");
 
@@ -48,4 +50,64 @@ List<string> devclass = new List<string>
             table2.AddRow(member);
 
         }
+        table2.Width(15);
         AnsiConsole.Write(table2);
+
+
+/*
+var table5 = new Table();
+table5.AddColumn("Nome");
+table5.AddColumn("Cognome");
+
+var partecipanti = new Dictionary<string, string> {
+    {"Mario", "Rossi"},
+    {"Luca", "Verdi"},
+    {"Andrea", "Bianchi"}
+};
+
+foreach (var member in partecipanti)
+{
+    table5.AddRow(member.Key, member.Value);
+}
+AnsiConsole.Write(table5);
+
+var table6 = new Table();  
+table6.AddColumn("Nome");
+table6.AddColumn("Cognome");
+table6.AddColumn("Anno di nascita");
+
+var partecipanti2 = new Dictionary<string, (string, int)>
+{
+    {"Mario", ("Rossi", 1990)},
+    {"Luca", ("Bianchi", 1980)},
+    {"Paolo", ("Verdi", 1970)}
+};
+
+foreach(var member in partecipanti2)
+{
+    table6.AddRow(member.Key, member.Value.Item1, member.Value.Item2.ToString());
+}
+
+AnsiConsole.Write(table6);
+
+var table7 = new Table();
+table7.AddColumn("Nome");
+table7.AddColumn("Soprannome");
+table7.AddColumn("Cognome");
+table7.AddColumn("Anno di nascita");
+
+
+var partecipanti3 = new Dictionary<(string, string), (string, int)>
+{
+    {("Mario","Ciccio"), ("Rossi", 1990)},
+    {("Luca", "gigetto"), ("Bianchi", 1980)},
+    {("Paolo", "nan"), ("Verdi", 1970)}
+};
+
+foreach (var member in partecipanti3)
+{
+    table7.AddRow(member.Key.Item1, member.Key.Item2, member.Value.Item1, member.Value.Item2.ToString());
+}
+
+AnsiConsole.Write(table7);
+*/
---
+++ esercitazioni/41-dadi-spectre/Program.cs
@@ -0,0 +1,80 @@
+﻿using Spectre.Console;
+
+Random random = new Random();
+int myScore = 100;
+int computerScore = 100;
+
+AnsiConsole.Write(new FigletText("Dice Game").Centered().Color(Color.Green));
+AnsiConsole.MarkupLine("[bold]Welcome to the dice game, you'll be playing against me![/]");
+
+// Function to display scores with bar charts
+void DisplayScores(int myScore, int computerScore)
+{
+    AnsiConsole.MarkupLine("[bold]Here are the new scores:[/]");
+    
+    var barChart = new BarChart()
+        .Width(60)
+        .Label("[bold]Scores[/]")
+        .CenterLabel();
+
+    barChart.AddItem("You", myScore, Color.Green);
+    barChart.AddItem("Me", computerScore, Color.Red);
+
+    AnsiConsole.Write(barChart);
+}
+
+// Initial Score Display
+DisplayScores(myScore, computerScore);
+
+while (myScore > 0 && computerScore > 0)
+{
+    AnsiConsole.MarkupLine("[bold]Press a button to throw the dices[/]");
+    Console.ReadKey(true);
+    AnsiConsole.WriteLine("");
+
+    int dice1 = random.Next(1, 7);
+    int dice2 = random.Next(1, 7);
+    int dice3 = random.Next(1, 7);
+    int dice4 = random.Next(1, 7);
+
+    // Display User Dice
+    var userPanel = new Panel($"[bold]Your dice:[/]\n[green]{dice1}[/], [green]{dice2}[/]");
+    userPanel.Header = new PanelHeader("[bold]Your Roll[/]", Justify.Center);
+    userPanel.Border = BoxBorder.Double;
+    userPanel.Padding = new Padding(1, 1, 1, 1);
+    AnsiConsole.Write(userPanel);
+
+    // Display Computer Dice
+    var computerPanel = new Panel($"[bold]My dice:[/]\n[green]{dice3}[/], [green]{dice4}[/]");
+    computerPanel.Header = new PanelHeader("[bold]My Roll[/]", Justify.Center);
+    computerPanel.Border = BoxBorder.Double;
+    computerPanel.Padding = new Padding(1, 1, 1, 1);
+    AnsiConsole.Write(computerPanel);
+
+    int myPoints = dice1 + dice2;
+    int computerPoints = dice3 + dice4;
+    int pointsDifference = Math.Abs(myPoints - computerPoints);
+
+    if (myPoints > computerPoints)
+    {
+        computerScore -= pointsDifference;
+        if (computerScore < 0) computerScore = 0;
+    }
+    else if (myPoints < computerPoints)
+    {
+        myScore -= pointsDifference;
+        if (myScore < 0) myScore = 0;
+    }
+
+    // Updated Score Display
+    DisplayScores(myScore, computerScore);
+}
+
+if (myScore <= 0)
+{
+    AnsiConsole.MarkupLine("[red bold]Sorry, I win![/]");
+}
+else if (computerScore <= 0)
+{
+    AnsiConsole.MarkupLine("[green bold]Congratulations!! You Win![/]");
+}
