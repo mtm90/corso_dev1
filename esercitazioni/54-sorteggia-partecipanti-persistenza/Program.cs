b8b9ecce0e73b13ba991f3772ffb9a437f04fc92
mtm90 <mattia22nchi@gmail.com>
2024-07-15 18:14:39 +0200

    comments

--- esercitazioni/54-sorteggia-partecipanti-persistenza/Program.cs
+++ esercitazioni/54-sorteggia-partecipanti-persistenza/Program.cs
@@ -1,8 +1,4 @@
-﻿using Spectre.Console;
-Console.Clear();
-        List<string> members = new List<string> { "Mattia", "Allison", "Silvano", "Ginevra", "Daniele", "Matteo", "Francesco", "Serghej" };
-        string path = @"members.txt";
-
+﻿// Check if the file exists and is not empty, then load members from the file
         if (File.Exists(path) && File.ReadAllText(path).Length != 0)
         {
             string[] savedMembers = File.ReadAllLines(path);
@@ -10,24 +6,29 @@ Console.Clear();
         }
         else
         {
+            // If the file does not exist or is empty, write the initial members to the file
             File.WriteAllLines(path, members);
         }
 
-bool programIsRunning = true; // creo una variabile booleana che definisce quando il programma deve funzionare
-while (programIsRunning)
-    {
-        AnsiConsole.WriteLine("");
-    var input = AnsiConsole.Prompt(
-    new SelectionPrompt<string>()
-        .Title("[bold]Members menu[/]")
-        .PageSize(9)
-        .AddChoices(new[] {
-            "add member", "show members", "order members", 
-            "find members", "delete members", "edit members",
-            "sort in teams", "sort in teams with GetRange", "Quit",
-        }));  
+        bool programIsRunning = true;
 
-            switch (input) 
+        // Main program loop
+        while (programIsRunning)
+        {
+            AnsiConsole.WriteLine("");
+            var input = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[bold]Members menu[/]")
+                    .PageSize(9)
+                    .AddChoices(new[]
+                    {
+                        "add member", "show members", "order members",
+                        "find members", "delete members", "edit members",
+                        "sort in teams", "sort in teams with GetRange", "Quit"
+                    }));
+
+            // Handle the user's menu selection
+            switch (input)
             {
                 case "add member":
                     Console.Clear();
@@ -41,8 +42,9 @@ while (programIsRunning)
                     {
                         members.Add(newMember);
                         File.AppendAllText(path, newMember + "\n");
-                    }               
+                    }
                     break;
+
                 case "show members":
                     Console.Clear();
                     var table = new Table();
@@ -52,13 +54,13 @@ while (programIsRunning)
                         table.AddRow(member);
                     }
                     AnsiConsole.Write(table);
-
                     Console.WriteLine($"Total members: {members.Count}");
                     break;
+
                 case "order members":
                     Console.Clear();
                     string sortInput = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
+                        new SelectionPrompt<string>()
                             .Title("Sort Menu")
                             .PageSize(3)
                             .AddChoices(new[] {
@@ -66,30 +68,32 @@ while (programIsRunning)
                                 "go back"
                             }));
                     switch (sortInput)
-                        {
-                            case "alphabetical order":
+                    {
+                        case "alphabetical order":
                             members.Sort();
                             break;
-                            case "alphabetical order inverted":
+                        case "alphabetical order inverted":
                             members.Sort();
                             members.Reverse();
                             break;
-                            case "go back":
+                        case "go back":
                             break;
-                        }                
-                break;        
+                    }
+                    break;
+
                 case "find members":
-                    Console.WriteLine("insert name:");
+                    Console.WriteLine("Insert name:");
                     string name = Console.ReadLine()!;
                     if (members.Contains(name))
                     {
                         Console.WriteLine("The name is listed");
                     }
-                    else 
+                    else
                     {
                         Console.WriteLine("The name is not listed");
                     }
                     break;
+
                 case "delete members":
                     Console.WriteLine("Type in the name of the member you want to delete");
                     string memberToDelete = Console.ReadLine()!;
@@ -98,14 +102,15 @@ while (programIsRunning)
                         members.Remove(memberToDelete);
                         Console.WriteLine("The name was deleted");
                         File.WriteAllLines(path, members);
-
                     }
-                    else {
+                    else
+                    {
                         Console.WriteLine("The name is not listed");
                     }
                     break;
+
                 case "edit members":
-                    Console.WriteLine("member name:");
+                    Console.WriteLine("Member name:");
                     name = Console.ReadLine()!;
                     if (members.Contains(name))
                     {
@@ -113,28 +118,27 @@ while (programIsRunning)
                         string newName = Console.ReadLine()!;
                         int index = members.IndexOf(name);
                         members[index] = newName;
-                        Console.WriteLine("the member was successfully edited");
+                        Console.WriteLine("The member was successfully edited");
                         File.WriteAllLines(path, members);
                     }
-                    else 
-                    { 
-                        Console.WriteLine("the member is not in list");
+                    else
+                    {
+                        Console.WriteLine("The member is not in the list");
                     }
-                break;
+                    break;
+
                 case "sort in teams":
                     Console.Clear();
                     Random mix = new();
                     bool addToteam1 = true;
-                    var team1Table = new Table();
-                    team1Table.Width = 15;
-                    team1Table.AddColumn("[bold]Team 1[/]");
-                    var team2Table = new Table();
-                    team2Table.Width = 15;
-                    team2Table.AddColumn("[bold]Team 2[/]");
+                    var team1Table = new Table().AddColumn("[bold]Team 1[/]");
+                    var team2Table = new Table().AddColumn("[bold]Team 2[/]");
+
+                    // Randomly distribute members into two teams
                     while (members.Count > 0)
                     {
                         int randomIndex = mix.Next(0, members.Count);
-                        if(addToteam1)
+                        if (addToteam1)
                         {
                             team1Table.AddRow(members[randomIndex]);
                             addToteam1 = false;
@@ -148,19 +152,20 @@ while (programIsRunning)
                     }
                     AnsiConsole.Write(team1Table);
                     AnsiConsole.Write(team2Table);
+
+                    // Reload members from the file after sorting into teams
                     members.AddRange(File.ReadAllLines(path));
                     break;
+
                 case "sort in teams with GetRange":
                     Console.Clear();
-                    int split = members.Count/2;
-                    List<string> team1 = members.GetRange(0, split);    
+                    int split = members.Count / 2;
+                    List<string> team1 = members.GetRange(0, split);
                     List<string> team2 = members.GetRange(split, members.Count - split);
-                    var team1TableWithGetRange = new Table();
-                    team1TableWithGetRange.Width = 15;
-                    team1TableWithGetRange.AddColumn("[bold]Team 1[/]");
-                    var team2TableWithGetRange = new Table();
-                    team2TableWithGetRange.Width = 15;
-                    team2TableWithGetRange.AddColumn("[bold]Team 2[/]");
+                    var team1TableWithGetRange = new Table().AddColumn("[bold]Team 1[/]");
+                    var team2TableWithGetRange = new Table().AddColumn("[bold]Team 2[/]");
+
+                    // Display teams
                     foreach (string member in team1)
                     {
                         team1TableWithGetRange.AddRow(member);
@@ -170,11 +175,14 @@ while (programIsRunning)
                         team2TableWithGetRange.AddRow(member);
                     }
                     AnsiConsole.Write(team1TableWithGetRange);
-                    AnsiConsole.Write(team2TableWithGetRange);  
+                    AnsiConsole.Write(team2TableWithGetRange);
                     break;
+
                 case "Quit":
                     programIsRunning = false;
                     break;
             }
-    }
-    File.WriteAllLines(path, members);
+        }
+
+        // Save the members list to the file before exiting
+        File.WriteAllLines(path, members);
