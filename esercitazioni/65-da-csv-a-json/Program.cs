979dc5b7ee6be83f8e51a6e640fd422bdef6e342
mtm90 <mattia22nchi@gmail.com>
2024-07-17 15:14:00 +0200

    new json and csv stuff

---
+++ esercitazioni/65-da-csv-a-json/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+class  Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.csv";
+        string[] lines = File.ReadAllLines(path);
+        string[][] prodotti = new string[lines.Length][];
+        for (int i = 0;i < lines.Length;i++)
+        {
+            prodotti[i] = lines[i].Split(',');
+        }
+        for (int i =1; i < prodotti.Length; i++)
+        {
+            string path2 = prodotti[i][0] + ".json";
+            File.Create(path2).Close();
+            File.AppendAllText(path2, JsonConvert.SerializeObject(new {nome = prodotti[i][0], prezzo = prodotti[i][1]}));
+        }        
+    }
+}
---
+++ esercitazioni/66-creazione-JSON-con-interfaccia/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.json"; 
+        File.Create(path).Close();
+        File.AppendAllText(path, "[\n"); // scrive la riga nel file
+        
+
+        while (true)
+        {
+            Console.WriteLine("Inserisci nome e prezzo");
+            string nome = Console.ReadLine()!.Trim(); // legge il nome
+            string prezzo = Console.ReadLine()!.Trim(); // legge il prezzo
+            File.AppendAllText(path, JsonConvert.SerializeObject(new {nome, prezzo = prezzo.ToString() }) + ",\n"); // scrive la riga nel file
+            Console.WriteLine("Vuoi inserire un altro prodotto? (s/n)");
+            string risposta = Console.ReadLine()!;
+            if (risposta.Equals("n"))
+            {
+                break;
+            }
+        }
+        
+        // togli l'ultima virgola
+        string file = File.ReadAllText(path);
+        file = file.Remove(file.Length - 2, 1); // gli argomenti -2 e 1 sono rispettivamente la posizione e il numero di caratteri da rimuovere dalla stringa
+
+        File.WriteAllText(path, file);
+        File.AppendAllText(path, "]"); // scrive la riga nel file
+    }
+}
