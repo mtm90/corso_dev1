146b4074e3c5c3f13f53236fb4ac9019ee59e63f
mtm90 <mattia22nchi@gmail.com>
2024-07-19 15:49:25 +0200

    implemented hand values to determine winner

--- esercitazioni/68-Poker-App/Program.cs
+++ esercitazioni/68-Poker-App/Program.cs
@@ -7,6 +7,7 @@ class Program
 {
     static string[] deck = new string[52];
     static string[] playerHand = new string[2];
+    static string[] computerHand = new string[2];
     static string[] communityCards = new string[5];
     static int currentCardIndex = 0;
     static int playerStack = 200;
@@ -78,11 +79,11 @@ class Program
 
             if (isPlayerSmallBlind)
             {
-                handEnded = !PlayerAction() || !ComputerAction();
+                handEnded = !BettingRound(true);
             }
             else
             {
-                handEnded = !ComputerAction() || !PlayerAction();
+                handEnded = !BettingRound(false);
             }
 
             if (handEnded)
@@ -96,14 +97,7 @@ class Program
             DisplayCommunityCards();
             DisplayPot();
 
-            if (isPlayerSmallBlind)
-            {
-                handEnded = !PlayerAction() || !ComputerAction();
-            }
-            else
-            {
-                handEnded = !ComputerAction() || !PlayerAction();
-            }
+            handEnded = !BettingRound(isPlayerSmallBlind);
 
             if (handEnded)
             {
@@ -116,14 +110,7 @@ class Program
             DisplayCommunityCards();
             DisplayPot();
 
-            if (isPlayerSmallBlind)
-            {
-                handEnded = !PlayerAction() || !ComputerAction();
-            }
-            else
-            {
-                handEnded = !ComputerAction() || !PlayerAction();
-            }
+            handEnded = !BettingRound(isPlayerSmallBlind);
 
             if (handEnded)
             {
@@ -136,21 +123,15 @@ class Program
             DisplayCommunityCards();
             DisplayPot();
 
-            if (isPlayerSmallBlind)
-            {
-                handEnded = !PlayerAction() || !ComputerAction();
-            }
-            else
-            {
-                handEnded = !ComputerAction() || !PlayerAction();
-            }
+            handEnded = !BettingRound(isPlayerSmallBlind);
 
             if (handEnded)
             {
                 EndHand();
                 continue;
             }
-
+                    Console.WriteLine($"Card 1: {computerHand[0]}");
+                    Console.WriteLine($"Card 2: {computerHand[1]}");
             DetermineWinner();
             EndHand();
         }
@@ -159,6 +140,41 @@ class Program
         Console.WriteLine($"Final Player Stack: {playerStack}");
         Console.WriteLine($"Final Computer Stack: {computerStack}");
         WaitForUserInput();
+    }
+
+    static bool BettingRound(bool playerStarts)
+    {
+        bool actionTaken = false;
+        while (true)
+        {
+            if (playerStarts)
+            {
+                actionTaken = PlayerAction();
+                if (!actionTaken || playerBet == computerBet)
+                {
+                    break;
+                }
+                actionTaken = ComputerAction();
+                if (!actionTaken || playerBet == computerBet)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                actionTaken = ComputerAction();
+                if (!actionTaken || playerBet == computerBet)
+                {
+                    break;
+                }
+                actionTaken = PlayerAction();
+                if (!actionTaken || playerBet == computerBet)
+                {
+                    break;
+                }
+            }
+        }
+        return actionTaken;
     }
 
     static void EndHand()
@@ -188,7 +204,7 @@ class Program
         {
             for (int j = 0; j < values.Length; j++)
             {
-                deck[index] = $"{values[j]} of {suits[i]}";
+                deck[index] = $"{values[j]}{suits[i]}";
                 index++;
             }
         }
@@ -215,6 +231,8 @@ class Program
     {
         playerHand[0] = DealCard();
         playerHand[1] = DealCard();
+        computerHand[0] = DealCard();
+        computerHand[1] = DealCard();
     }
 
     static void DealCommunityCards(int stage)
@@ -260,7 +278,7 @@ class Program
         Console.WriteLine("Enter your action: (1) Bet (2) Raise (3) Call (4) Check (5) Fold");
 
         int action = Convert.ToInt32(Console.ReadLine());
-        int callAmount = (isPlayerSmallBlind ? bigBlind : playerBet) - playerBet;
+        int callAmount = computerBet - playerBet;
 
         switch (action)
         {
@@ -285,10 +303,10 @@ class Program
                 }
                 playerStack -= callAmount;
                 pot += callAmount;
-                playerBet = bigBlind; // Assuming the player is calling the big blind
+                playerBet = computerBet;
                 break;
             case 4: // Check
-                if (playerBet == bigBlind || playerBet == 0)
+                if (playerBet == computerBet)
                 {
                     Console.WriteLine("Checked.");
                 }
@@ -299,7 +317,7 @@ class Program
                 }
                 break;
             case 5: // Fold
-                Console.WriteLine("You folded.");
+                Console.WriteLine("Player folds.");
                 computerStack += pot; // Computer wins the pot
                 pot = 0;
                 return false; // Indicates hand is over
@@ -312,10 +330,9 @@ class Program
 
     static bool ComputerAction()
     {
-        // Simple computer logic (for demonstration purposes)
         Random rand = new Random();
         int action = rand.Next(1, 6);
-        int callAmount = (isPlayerSmallBlind ? bigBlind : playerBet) - computerBet;
+        int callAmount = playerBet - computerBet;
 
         switch (action)
         {
@@ -325,6 +342,7 @@ class Program
                 computerStack -= amount;
                 pot += amount;
                 computerBet += amount;
+                Console.WriteLine($"Computer bets/raises {amount}");
                 break;
             case 3: // Call
                 if (callAmount > computerStack)
@@ -333,10 +351,11 @@ class Program
                 }
                 computerStack -= callAmount;
                 pot += callAmount;
-                computerBet = bigBlind;
+                computerBet = playerBet;
+                Console.WriteLine("Computer calls");
                 break;
             case 4: // Check
-                if (computerBet == bigBlind || computerBet == 0)
+                if (computerBet == playerBet)
                 {
                     Console.WriteLine("Computer checks.");
                 }
@@ -347,7 +366,7 @@ class Program
                 }
                 break;
             case 5: // Fold
-                Console.WriteLine("Computer folded.");
+                Console.WriteLine("Computer folds.");
                 playerStack += pot; // Player wins the pot
                 pot = 0;
                 return false; // Indicates hand is over
@@ -384,20 +403,133 @@ class Program
 
     static void DetermineWinner()
     {
-        // For simplicity, randomly choose the winner. 
-        // A full implementation should evaluate the best hand between player and computer
-        Random rand = new Random();
-        int winner = rand.Next(1, 3); // 1 for Player, 2 for Computer
-        if (winner == 1)
+        var playerBestHand = GetBestHand(playerHand.Concat(communityCards).ToArray());
+        var computerBestHand = GetBestHand(computerHand.Concat(communityCards).ToArray());
+
+        int comparison = CompareHands(playerBestHand, computerBestHand);
+
+        if (comparison > 0)
         {
             Console.WriteLine("Player wins!");
             playerStack += pot;
         }
-        else
+        else if (comparison < 0)
         {
             Console.WriteLine("Computer wins!");
             computerStack += pot;
         }
+        else
+        {
+            Console.WriteLine("It's a tie!");
+            playerStack += pot / 2;
+            computerStack += pot / 2;
+        }
+    }
+
+    static (string Rank, List<int> Values) GetBestHand(string[] cards)
+    {
+        // Parse the cards into values and suits
+        var parsedCards = cards.Select(card => (
+            Value: "23456789TJQKA".IndexOf(card[0]) + 2,
+            Suit: card[1]
+        )).ToList();
+
+        parsedCards.Sort((a, b) => b.Value.CompareTo(a.Value));
+
+        // Group by value and by suit
+        var groupsByValue = parsedCards.GroupBy(card => card.Value).OrderByDescending(group => group.Count()).ThenByDescending(group => group.Key).ToList();
+        var groupsBySuit = parsedCards.GroupBy(card => card.Suit).ToList();
+
+        bool isFlush = groupsBySuit.Any(group => group.Count() >= 5);
+        bool isStraight = false;
+        List<int> straightHighCards = new List<int>();
+
+        for (int i = 0; i <= parsedCards.Count - 5; i++)
+        {
+            if (parsedCards[i].Value - 4 == parsedCards[i + 4].Value &&
+                parsedCards[i].Value - 1 == parsedCards[i + 1].Value &&
+                parsedCards[i].Value - 2 == parsedCards[i + 2].Value &&
+                parsedCards[i].Value - 3 == parsedCards[i + 3].Value)
+            {
+                isStraight = true;
+                straightHighCards.Add(parsedCards[i].Value);
+                break;
+            }
+        }
+
+        if (!isStraight && parsedCards[0].Value == 14)
+        {
+            var lowStraight = new List<int> { 5, 4, 3, 2, 14 };
+            if (parsedCards.Select(card => card.Value).Intersect(lowStraight).Count() == 5)
+            {
+                isStraight = true;
+                straightHighCards.Add(5);
+            }
+        }
+
+        if (isStraight && isFlush)
+        {
+            return ("Straight Flush", straightHighCards);
+        }
+
+        if (groupsByValue[0].Count() == 4)
+        {
+            return ("Four of a Kind", groupsByValue.SelectMany(group => group).Take(5).Select(card => card.Value).ToList());
+        }
+
+        if (groupsByValue[0].Count() == 3 && groupsByValue[1].Count() >= 2)
+        {
+            return ("Full House", groupsByValue.SelectMany(group => group).Take(5).Select(card => card.Value).ToList());
+        }
+
+        if (isFlush)
+        {
+            return ("Flush", groupsBySuit.First(group => group.Count() >= 5).Select(card => card.Value).Take(5).ToList());
+        }
+
+        if (isStraight)
+        {
+            return ("Straight", straightHighCards);
+        }
+
+        if (groupsByValue[0].Count() == 3)
+        {
+            return ("Three of a Kind", groupsByValue.SelectMany(group => group).Take(5).Select(card => card.Value).ToList());
+        }
+
+        if (groupsByValue[0].Count() == 2 && groupsByValue[1].Count() == 2)
+        {
+            return ("Two Pair", groupsByValue.SelectMany(group => group).Take(5).Select(card => card.Value).ToList());
+        }
+
+        if (groupsByValue[0].Count() == 2)
+        {
+            return ("One Pair", groupsByValue.SelectMany(group => group).Take(5).Select(card => card.Value).ToList());
+        }
+
+        return ("High Card", parsedCards.Take(5).Select(card => card.Value).ToList());
+    }
+
+    static int CompareHands((string Rank, List<int> Values) hand1, (string Rank, List<int> Values) hand2)
+    {
+        string[] handRanks = { "High Card", "One Pair", "Two Pair", "Three of a Kind", "Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush" };
+        int rank1 = Array.IndexOf(handRanks, hand1.Rank);
+        int rank2 = Array.IndexOf(handRanks, hand2.Rank);
+
+        if (rank1 != rank2)
+        {
+            return rank1.CompareTo(rank2);
+        }
+
+        for (int i = 0; i < hand1.Values.Count; i++)
+        {
+            if (hand1.Values[i] != hand2.Values[i])
+            {
+                return hand1.Values[i].CompareTo(hand2.Values[i]);
+            }
+        }
+
+        return 0;
     }
 
     static void WaitForUserInput()
