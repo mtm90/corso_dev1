cda9a9474d494d902e71cb02a333b6290b8e17c1
mtm90 <mattia22nchi@gmail.com>
2024-10-18 18:00:28 +0200

    chat app

---
+++ esercitazioni/chat/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Client
+{
+    private TcpClient client;
+    private NetworkStream stream;
+
+    public void StartClient(string serverIP, int port)
+    {
+        try
+        {
+            client = new TcpClient(serverIP, port);
+            stream = client.GetStream();
+            Console.WriteLine("Connected to server.");
+
+            Thread receiveThread = new Thread(ReceiveMessages);
+            receiveThread.Start();
+
+            SendMessages();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+        finally
+        {
+            stream?.Close();
+            client?.Close();
+        }
+    }
+
+    private void SendMessages()
+    {
+        string messageToSend;
+        while ((messageToSend = Console.ReadLine()) != null && messageToSend != "")
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(messageToSend);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+    private void ReceiveMessages()
+    {
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        try
+        {
+            while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string message = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                Console.WriteLine("Server: " + message);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Connection closed: " + e.Message);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the server IP:");
+        string serverIP = Console.ReadLine();
+
+        Client client = new Client();
+        client.StartClient(serverIP, 3000);
+    }
+}
---
+++ esercitazioni/chatServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+public class Server
+{
+    private TcpListener listener;
+    private List<TcpClient> clients = new List<TcpClient>();
+
+    public void StartServer(int port)
+    {
+        listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine("Server started on port " + port);
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            lock (clients)
+            {
+                clients.Add(client);
+            }
+
+            Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+            clientThread.Start(client);
+        }
+    }
+
+    private void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+        {
+            string message = Encoding.ASCII.GetString(buffer, 0, byteCount);
+            Console.WriteLine("Received: " + message);
+            Broadcast(message);
+        }
+
+
+    }
+
+    private void Broadcast(string message)
+    {
+        byte[] buffer = Encoding.ASCII.GetBytes(message);
+    }
+
+    public static void Main(string[] args)
+    {
+        Server server = new Server();
+        server.StartServer(3000);
+    }
+}
